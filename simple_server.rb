d9804c86ba2ec33e35a011a99f0870674df1ed57
Alejandro Escobar <alejoescobac@gmail.com>
2015-03-18 10:22:35 -0500

    Start echo server

--- simple_server.rb
+++ simple_server.rb
@@ -4,6 +4,8 @@ server = TCPServer.open(2000)  # Socket to listen on port 2000
 loop {                         # Servers run forever
   client = server.accept       # Wait for a client to connect
   client.puts(Time.now.ctime)  # Send the time to the client
+  puts "received:" + client.recv(1024)
+  client.write 'got something--closing now--here is your response message from the server'
   client.puts "Closing the connection. Bye!"
   client.close                 # Disconnect from the client
 }
