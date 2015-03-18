6648e57855a5ad34116a5bbf7e592deb060b1662
Alejandro Escobar <alejoescobac@gmail.com>
2015-03-18 11:33:11 -0500

    Finish server

--- simple_server.rb
+++ simple_server.rb
@@ -1,16 +1,25 @@
 require 'socket'               # Get sockets from stdlib
 
-server = TCPServer.open(2000)  # Socket to listen on port 2000
-loop {                         # Servers run forever
-  client = server.accept       # Wait for a client to connect
-  client.puts(Time.now.ctime)  # Send the time to the client
+counter = 0 
+server = TCPServer.open(2000)
+client = server.accept   # Socket to listen on port 2000
+loop {                         # Servers run forever      # Wait for a client to connect
+  # Send the time to the client
   message = client.recv(1024)
+  puts message
   if message == "PING"
+    counter += 1
     puts client.write "PONG\n"
-  elsif message == "MSG"
-    puts "hallo"
+    puts counter
+  elsif message[0...3] == "MSG"
+    puts ''
+    puts message[3..-1]
     puts client.write "OK\n"
+  elsif message == "BYE"
+    client.puts "Closing the connection. Bye!"
+    client.close              # Disconnect from the client
+  else 
+    puts client.write "BADREQUEST"
   end
-  client.puts "Closing the connection. Bye!"
-  client.close                 # Disconnect from the client
+                
 }
