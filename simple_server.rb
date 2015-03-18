de76e05cb878dd9343d75b97e409323bd3d321b4
Alejandro Escobar <alejoescobac@gmail.com>
2015-03-18 10:42:42 -0500

    add pong and msg conditional

--- simple_server.rb
+++ simple_server.rb
@@ -4,8 +4,13 @@ server = TCPServer.open(2000)  # Socket to listen on port 2000
 loop {                         # Servers run forever
   client = server.accept       # Wait for a client to connect
   client.puts(Time.now.ctime)  # Send the time to the client
-  puts "received:" + client.recv(1024)
-  client.write 'got something--closing now--here is your response message from the server'
+  message = client.recv(1024)
+  if message == "PING"
+    puts client.write "PONG\n"
+  elsif message == "MSG"
+    puts "hallo"
+    puts client.write "OK\n"
+  end
   client.puts "Closing the connection. Bye!"
   client.close                 # Disconnect from the client
 }
